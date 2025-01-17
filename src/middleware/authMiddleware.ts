0b524a4d22a6f856556496d455c72d1e165cadda
egorb <egorbobylev398@gmail.com>
2025-01-17 23:04:59 +0500

    upd

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,30 +1,32 @@
+
 import { Context } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 interface JwtPayload {
-  email: string;
-  password?: string; // password is not available on the jwt
-  role: "user" | "admin";
+    email: string;
+    password?: string; // password is not available on the jwt
+    role: "user" | "admin";
 }
 
 export const authorizeAdmin = () => ({
-  async beforeHandle({ cookie:{auth}, set, jwt }: Context) {
-    // Get the JWT from the cookie
-    const token = auth.value  
+    async beforeHandle({ cookie, set, jwt }: Context) {
+      // Get the JWT from the cookie
+        const token = cookie.get("auth");
 
-    if (!token) {
-      set.status = 401;
-      return "Unauthorized: No authentication token found";
-    }
-    // Decode the JWT, and check the role
-    try {
-      const payload = await jwt.verify<JwtPayload>(token);
-      if (!payload || payload.role !== "admin") {
-        set.status = 403;
-        return "Forbidden: Insufficient privileges";
+        if (!token) {
+            set.status = 401;
+            return "Unauthorized: No authentication token found";
+        }
+      // Decode the JWT, and check the role
+      try{
+         const payload = await jwt.verify<JwtPayload>(token);
+         if(!payload || payload.role !== "admin") {
+             set.status = 403
+             return "Forbidden: Insufficient privileges";
+         }
+      } catch (error) {
+          set.status = 401
+          return "Invalid authentication token"
       }
-    } catch (error) {
-      set.status = 401;
-      return "Invalid authentication token";
-    }
-  },
+    },
 });
+
