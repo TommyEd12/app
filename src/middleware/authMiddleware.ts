42ba3d610788ed3d46bc48c3d908093952e80683
egorb <egorbobylev398@gmail.com>
2025-01-17 22:52:43 +0500

    middleware upd

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,15 +1,30 @@
+import { Context } from "elysia";
+import { jwt } from "@elysiajs/jwt";
+interface JwtPayload {
+  email: string;
+  password?: string; // password is not available on the jwt
+  role: "user" | "admin";
+}
+
 export const authorizeAdmin = () => ({
-    async beforeHandle({ cookie, set }) {
-        const role = cookie.get("role");
+  async beforeHandle({ cookie, set, jwt }: Context) {
+    // Get the JWT from the cookie
+    const token = cookie.get("auth");
 
-        if (!role) {
-            set.status = 401;
-            return "Unauthorized: No role cookie found";
-        }
-        if (role !== "admin") {
-            set.status = 403;
-            return "Forbidden: Insufficient privileges";
-        }
-    },
+    if (!token) {
+      set.status = 401;
+      return "Unauthorized: No authentication token found";
+    }
+    // Decode the JWT, and check the role
+    try {
+      const payload = await jwt.verify<JwtPayload>(token);
+      if (!payload || payload.role !== "admin") {
+        set.status = 403;
+        return "Forbidden: Insufficient privileges";
+      }
+    } catch (error) {
+      set.status = 401;
+      return "Invalid authentication token";
+    }
+  },
 });
-
